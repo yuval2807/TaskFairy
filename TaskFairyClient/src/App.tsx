198f69da5f3c130da778a510e0af7595783d4f46
Yuval Hikry <hashlama53@bsmch.net>
2025-01-03 19:45:05 +0200

    initial project

--- TaskFairyClient/src/App.tsx
+++ TaskFairyClient/src/App.tsx
@@ -1,19 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import LoginForm from "./views/LoginPage";
-import Registration from "./views/Registration";
-import Router from "./Router";
-const handleRegister = async (data: any) => {
-  // Add registration logic here
-  console.log(data);
-};
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      first app
     </div>
   );
 }
--- TaskFairyClient/src/views/HomePage/HomePage.tsx
+++
@@ -1,5 +0,0 @@
-const HomePage: React.FC = () => {
-  return <div className='App'> first page</div>;
-};
-
-export default HomePage;
